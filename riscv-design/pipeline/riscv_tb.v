cb36bb38d8f23f647a4872a82120853043fc99ed
khizaralishahcs <khizaralishahcs07@gmail.com>
2024-03-17 15:28:38 +0500

    thinking of putting binary image in data memory of riscv

--- riscv-design/pipeline/riscv_tb.v
+++ riscv-design/pipeline/riscv_tb.v
@@ -2,9 +2,12 @@
 
 module riscv_tb;
 
+    parameter MEMORY_DEPTH = 6'b10_0000; // 32 in decimal
+
     reg clk, rst;
 
     integer i;
+    integer verbose;
 
     riscv core(clk, rst);
 
@@ -18,6 +21,7 @@ module riscv_tb;
             //$readmemh({"./set-instructions/test-", test_case_number, ".hex"}, core.insmem.memfile);
             $readmemh("./set-instructions/corrected-test-21.hex", core.insmem.memfile);
 
+            verbose = 1;
 
             clk = 0;
 
@@ -27,6 +31,23 @@ module riscv_tb;
             #2
             rst = 0;
 
-            #2000 $finish;
+            #2000 
+            
+            if (verbose > 0)
+            begin
+                
+                for(i=0; i < MEMORY_DEPTH; i=i+1)
+                begin
+                    $display("INSTRUCTION MEMORY[%2d] = %8b", i, core.insmem.memfile[i]);
+                end
+
+                for(i=0; i < MEMORY_DEPTH; i=i+1)
+                begin
+                    $display("DATA MEMORY[%2d] = %8b", i, core.datamem.memfile[i]);
+                end
+            end
+
+            #5
+            $finish;
         end
 endmodule
